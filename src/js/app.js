30fe4a5716ec5d6961cf5c6edbf5d35a825dd917
Manuel Contreras <manuelconlo@gmail.com>
2023-09-27 12:29:47 -0600

    otros cambios

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container")
+
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker
+	.register("serviceWorker.js")
+	.then(registration => console.log("service worker registered"))
+	.catch(err => console.log("service worker not registered", err))
+  }
