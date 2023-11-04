1897edb526d6fa00f624ec4b0ae397a43b4a176a
elcrack777 <76188356+elcrack777@users.noreply.github.com>
2023-11-03 21:34:29 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 const container = document.querySelector(".container")
 
+const coffees = [
+  {
+    name: "Explorer",
+    image: "images/explorer.png"
+  },
+  {
+    name: "Raptor",
+    image: "images/raptor.png"
+  },
+  {
+    name: "Spark",
+    image: "images/spark.png"
+  },
+  {
+    name: "Vento",
+    image: "images/vento.png"
+  }
+
+]
+
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output;
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
+
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function() {
 		navigator.serviceWorker
